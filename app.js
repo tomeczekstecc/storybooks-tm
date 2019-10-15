a0d2a39f149195f19038ce57af9f8c5a6407a4da
Tomasz Steć <tomeczekstecc@gmail.com>
2019-10-15 20:28:25 +0200

    Initial

---
+++ app.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const mongoose = require('mongoose');
+
+const app = express();
+
+app.get('/', (req, res) => {
+  res.send('Hello world');
+});
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () =>
+  console.log(`Server started on port ${PORT}. Go to http://localhost:${PORT}`)
+);
