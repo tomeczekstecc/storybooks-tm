871479c23e8da5eab87710da3205ce0c50cdccab
Tomasz Steć <tomeczekstecc@gmail.com>
2019-10-15 20:35:17 +0200

    ss

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const mongoose = require('mongoose');
 const app = express();
 
 app.get('/', (req, res) => {
-  res.send('Hello world');
+  res.send('Hello world!');
 });
 
 const PORT = process.env.PORT || 5000;
